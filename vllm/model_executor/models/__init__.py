1f826ff77994298aceecf38f7403f0de31f0fd59
huangshaomang <huangshaomang@360.cn>
2024-03-14 20:26:51 +0800

    Add: 增加对Hongshu大模型的支持

--- vllm/model_executor/models/__init__.py
+++ vllm/model_executor/models/__init__.py
@@ -46,6 +46,7 @@ _MODELS = {
     "StableLMEpochForCausalLM": ("stablelm", "StablelmForCausalLM"),
     "StableLmForCausalLM": ("stablelm", "StablelmForCausalLM"),
     "Starcoder2ForCausalLM": ("starcoder2", "Starcoder2ForCausalLM"),
+    "HongshuLMHeadModel": ("hongshu", "HongshuLMHeadModel"),
 }
 
 # Models not supported by ROCm.
